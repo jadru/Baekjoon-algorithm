89a4fc96774a05c841c063d5254d09ec1a78be4b
Younggun Park <ygpark0922@gmail.com>
2021-03-24 03:00:24 +0900

    make folder

---
+++ c++/3052.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int s[11], f[11];
+    for(int i = 0; i < 10; i++){
+        cin >> s[i];
+    }
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < 10; j++){
+            if(s[i])
+        }
+    }
+
+    return 0;
+} 
